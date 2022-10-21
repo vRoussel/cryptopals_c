9855667a5de5c35d4e9e583160df82f7b046fef9
Valentin Roussel <valentin.rssl@gmail.com>
2022-10-22 01:50:01 +0200

    add hex_to_base64 tests

--- libs/utils/test/test_utils.c
+++ libs/utils/test/test_utils.c
@@ -4,7 +4,23 @@
 void setUp() {}
 void tearDown() {}
 
-void test_cryptopals()
+void test_hex_to_base64_wrong_hex()
+{
+    char tmp[512];
+    int ret = hex_to_base64("AAG", tmp);
+    TEST_ASSERT_EQUAL_INT(ret, -1);
+    TEST_ASSERT_EQUAL_STRING(tmp, "");
+}
+
+void test_hex_to_base64_empty()
+{
+    char tmp[512];
+    int ret = hex_to_base64("", tmp);
+    TEST_ASSERT_EQUAL_INT(ret, 0);
+    TEST_ASSERT_EQUAL_STRING(tmp, "");
+}
+
+void test_hex_to_base64_cryptopals()
 {
     char tmp[512];
     int ret = hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", tmp);
@@ -16,6 +32,8 @@ void test_cryptopals()
 int main()
 {
     UNITY_BEGIN();
-    RUN_TEST(test_cryptopals);
+    RUN_TEST(test_hex_to_base64_empty);
+    RUN_TEST(test_hex_to_base64_wrong_hex);
+    RUN_TEST(test_hex_to_base64_cryptopals);
     return UNITY_END();
 }
