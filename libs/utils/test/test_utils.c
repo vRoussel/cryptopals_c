61de32cdba2f5461ca4779cdc6c8172c06186d05
Valentin Roussel <valentin.rssl@gmail.com>
2022-10-22 01:50:29 +0200

    add hex/byte conversion tests

--- libs/utils/test/test_utils.c
+++ libs/utils/test/test_utils.c
@@ -4,6 +4,49 @@
 void setUp() {}
 void tearDown() {}
 
+void test_hex_char_to_byte()
+{
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('-'), -1);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('G'), -1);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('0'), 0);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('1'), 1);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('2'), 2);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('3'), 3);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('4'), 4);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('5'), 5);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('6'), 6);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('7'), 7);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('8'), 8);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('9'), 9);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('A'), 10);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('B'), 11);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('C'), 12);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('D'), 13);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('E'), 14);
+    TEST_ASSERT_EQUAL_INT(hex_char_to_byte('F'), 15);
+}
+
+void test_byte_to_hex_char()
+{
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(0), '0');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(1), '1');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(2), '2');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(3), '3');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(4), '4');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(5), '5');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(6), '6');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(7), '7');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(8), '8');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(9), '9');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(10), 'A');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(11), 'B');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(12), 'C');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(13), 'D');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(14), 'E');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(15), 'F');
+    TEST_ASSERT_EQUAL_CHAR(byte_to_hex_char(16), -1);
+}
+
 void test_hex_to_base64_wrong_hex()
 {
     char tmp[512];
@@ -32,6 +75,8 @@ void test_hex_to_base64_cryptopals()
 int main()
 {
     UNITY_BEGIN();
+    RUN_TEST(test_hex_char_to_byte);
+    RUN_TEST(test_byte_to_hex_char);
     RUN_TEST(test_hex_to_base64_empty);
     RUN_TEST(test_hex_to_base64_wrong_hex);
     RUN_TEST(test_hex_to_base64_cryptopals);
