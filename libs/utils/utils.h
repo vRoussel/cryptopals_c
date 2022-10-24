3ce2adfd5dcb843f6b6a364ba34ff06425724df6
Valentin Roussel <valentin.rssl@gmail.com>
2022-10-24 15:53:37 +0200

    add missing funcs to utils header

--- libs/utils/utils.h
+++ libs/utils/utils.h
@@ -3,6 +3,8 @@
 
 int8_t hex_char_to_byte(char c);
 char byte_to_hex_char(uint8_t b);
+ssize_t decode_hex(char *input, uint8_t *output);
+ssize_t encode_hex(uint8_t *input, size_t input_len, char *output);
 ssize_t hex_to_base64(char *input, char *output);
 ssize_t xor(uint8_t *left, size_t left_len, uint8_t *right, size_t right_len, uint8_t *output);
 ssize_t xor_hex(char *left, char *right, char *output);
