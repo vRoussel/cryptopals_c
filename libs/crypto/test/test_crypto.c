33dc90571dc5768edf4cd2317bab94775d588fd6
Valentin Roussel <valentin.rssl@gmail.com>
2022-10-27 23:11:03 +0200

    add xor_repeated_key_str_to_hex func (cryptopals challenge 1.5)

--- libs/crypto/test/test_crypto.c
+++ libs/crypto/test/test_crypto.c
@@ -39,6 +39,18 @@ void test_xor()
 //    TEST_ASSERT_EQUAL_STRING(tmp, "ABBBCD");
 //}
 
+void test_xor_repeated_key_str_to_hex()
+{
+    char *input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+    char *key = "ICE";
+    char tmp[512];
+    ssize_t ret;
+
+    ret = xor_repeated_key_str_to_hex(input, key, tmp);
+    TEST_ASSERT_EQUAL_INT(ret, 148);
+    TEST_ASSERT_EQUAL_STRING(tmp, "0B3637272A2B2E63622C2E69692A23693A2A3C6324202D623D63343C2A26226324272765272A282B2F20430A652E2C652A3124333A653E2B2027630C692B20283165286326302E27282F");
+}
+
 void test_decipher_xor_single_byte_key()
 {
     char *ciphered = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
@@ -59,5 +71,6 @@ int main()
     UNITY_BEGIN();
     RUN_TEST(test_xor);
     RUN_TEST(test_decipher_xor_single_byte_key);
+    RUN_TEST(test_xor_repeated_key_str_to_hex);
     return UNITY_END();
 }
