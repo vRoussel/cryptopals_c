d74753826d03bc766d9351730a7dc1c3c3075f87
Valentin Roussel <valentin.rssl@gmail.com>
2022-11-03 21:24:19 +0100

    hamming_distance func takes bytes instead of chars

--- libs/utils/utils.c
+++ libs/utils/utils.c
@@ -1,6 +1,5 @@
 #include "utils.h"
 
-#include <stdint.h>
 #include <string.h>
 #include <limits.h>
 #include <stdbool.h>
@@ -9,17 +8,11 @@
 
 #include "crypto/crypto.h"
 
-unsigned int hamming_distance_str(const char* s1, const char* s2)
+unsigned int hamming_distance(const uint8_t *left, const uint8_t *right, size_t size)
 {
-    size_t s1_len = strlen(s1);
-    size_t s2_len = strlen(s2);
+    uint8_t xor_bytes[size];
 
-    if (s1_len != s2_len)
-        return -1;
-
-    uint8_t xor_bytes[s1_len];
-
-    ssize_t xor_ret = xor((uint8_t*)s1, s1_len, (uint8_t*)s2, s2_len, xor_bytes);
+    ssize_t xor_ret = xor(left, size, right, size, xor_bytes);
     if (xor_ret < 0)
         return -1;
 
@@ -33,6 +26,18 @@ unsigned int hamming_distance_str(const char* s1, const char* s2)
     }
 
     return diff_count;
+}
+
+unsigned int hamming_distance_str(const char* s1, const char* s2)
+{
+    size_t s1_len = strlen(s1);
+    size_t s2_len = strlen(s2);
+
+    if (s1_len != s2_len)
+        return -1;
+
+
+    return hamming_distance((uint8_t*)s1, (uint8_t*)s2, s1_len);
 }
 
 int get_next_line(char *out, size_t limit, unsigned int *line_num)
--- libs/utils/utils.h
+++ libs/utils/utils.h
@@ -1,9 +1,11 @@
 #include <stddef.h>
+#include <stdint.h>
 
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 #define SWAP(x, y, T) do { T SWAP = x; x = y; y = SWAP; } while (0)
 
+unsigned int hamming_distance(const uint8_t *left, const uint8_t *right, size_t size);
 unsigned int hamming_distance_str(const char* s1, const char* s2);
 int get_next_line(char *out, size_t limit, unsigned int *line_num);
 void skip_until_eol();
